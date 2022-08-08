148abc1e47649dca04c979acc9401c78fbbec386
MexoSa <27315902@mail.ru>
2022-08-08 14:30:26 +0300

    fix roundTo function

--- src/helpers/roundTo.ts
+++ src/helpers/roundTo.ts
@@ -1,3 +1,17 @@
 export const roundTo = (value: string) => {
-  return Math.trunc(Number(value) * 100) / 100
+  const number = +value
+
+  if (number < 0.1) {
+    const id = value.split('').findIndex(char => {
+      if (char === '.') {
+        return false
+      }
+      return +char != 0
+    })
+    return number.toFixed(id)
+  } else if (number < 1 && number > 0.1) {
+    return Math.round(number * 1000) / 1000
+  }
+
+  return number.toFixed(2)
 }
